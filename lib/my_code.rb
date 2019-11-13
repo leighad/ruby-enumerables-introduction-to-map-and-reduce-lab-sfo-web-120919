6679b1e61b9100eb5c4de69491351462d10af870
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-13 21:07:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,6 +8,13 @@ def map_to_negativize(array)
   #   i += -1
   # end
   # return neg_array
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 )
+    i += 1
+  end
+  return new
 end
 
 def map_to_no_change
