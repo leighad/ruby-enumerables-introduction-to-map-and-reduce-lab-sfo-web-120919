422344940cdb0e4c0bbed0a659cc28f6707f2c0e
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-13 21:39:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,6 +58,12 @@ def reduce_to_all_true(array)
   # end
   #   return true
   # end
+  i = 0
+  while i < array.length do
+    return false if !array[i]
+    i += 1
+  end
+  return true
 end
 
 def reduce_to_any_true(array)
