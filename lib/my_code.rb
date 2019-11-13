5f6e39d215e11f9cbdcf69fd4f5c4051069ba881
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-13 21:39:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,13 +51,13 @@ def reduce_to_total(array, start = 0)
 end
 
 def reduce_to_all_true(array)
-  i = 0 
-  while i < array.length do 
-    return false if array[i] != true
-    i += 1
-  end
-    return true
-  end
+  # i = 0 
+  # while i < array.length do 
+  #   return false if array[i] != true
+  #   i += 1
+  # end
+  #   return true
+  # end
 end
 
 def reduce_to_any_true(array)
