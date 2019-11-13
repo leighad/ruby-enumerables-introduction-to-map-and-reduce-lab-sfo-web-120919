e0e4ce5d554d2efc1717c2edd960d7caaf79a3d4
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-13 21:07:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,13 @@
 # My Code here.... **DO NOT USE MAP OR REDUCE**
 def map_to_negativize(array)
   # array.map { |el| el * -1}
-  neg_array = []
-  i = 0 
-  while i < array.length do
-    neg_array.push(array[i] * -1)
-    i += -1
-  end
-  return neg_array
+  # neg_array = []
+  # i = 0 
+  # while i < array.length do
+  #   neg_array.push(array[i] * -1)
+  #   i += -1
+  # end
+  # return neg_array
 end
 
 def map_to_no_change
